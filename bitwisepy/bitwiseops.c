b84f3506b7224bdd859a8ee9ed408eade186b021
Vishwajith K <vishuvikas1996@gmail.com>
2021-07-22 18:56:33 +0530

    add bitwise right shift related code
    Python code may now bitwise left shift, bitwise right shift
    it's numbers using exported C apis

--- bitwisepy/bitwiseops.c
+++ bitwisepy/bitwiseops.c
@@ -1,3 +1,7 @@
 int bit_lsh_i(int n, int s) {
 	return n << s;
 }
+
+int bit_rsh_i(int n, int s) {
+	return n >> s;
+}
--- bitwisepy/driver.py
+++ bitwisepy/driver.py
@@ -7,4 +7,9 @@ bitwise_lib = ctypes.CDLL(lib)
 bit_lsh_i = bitwise_lib.bit_lsh_i
 bit_lsh_i.restype = ctypes.c_int
 
-print(bit_lsh_i(5, 2))
+bit_rsh_i = bitwise_lib.bit_rsh_i
+bit_rsh_i.restype = ctypes.c_int
+
+print(bit_lsh_i(5, -2))
+print(bit_rsh_i(5, 2))
+
